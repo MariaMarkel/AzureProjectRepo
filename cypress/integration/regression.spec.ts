ae21666597d8a538f56bc8aedb826d8df5431701
MariaMarkel <volkova0512@gmail.com>
2022-01-17 19:24:50 -0500

    Initial commit

---
+++ cypress/integration/regression.spec.ts
@@ -0,0 +1,15 @@
+///<reference types = "Cypress"/>
+
+before(function (){
+
+})
+context('E2E Installer Portal: negative scenarios', function () {
+    it('SKU is required: cannot click Next', function () {
+
+    })
+})
+after (function () {
+
+})
+
+
